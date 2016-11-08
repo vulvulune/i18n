06fee63e6225db7b9265b29641655d1f2e88488f
zewa666 <zewa666@gmail.com>
2016-11-08 12:36:53 +0100

    feat(install): automatic updater for pacman
    defnition of import-hook for pacman in order
    to perform automatic installation and setup
    of Aurelia I18N when accepted by the user

---
+++ install/templates.js
@@ -0,0 +1,29 @@
+const IMPORT_SECTION = `import environment from './environment';
+import {I18N} from 'aurelia-i18n';
+import Backend from 'i18next-xhr-backend';`;
+
+const CONFIGURE_SECTION = `export function configure(aurelia) {
+  aurelia.use.plugin('aurelia-i18n', (instance) => {
+    instance.i18next.use(Backend);
+
+    return instance.setup({
+      backend: {                                 
+      loadPath: './locales/{{lng}}/{{ns}}.json',
+      },
+      lng : 'de',
+      attributes : ['t','i18n'],
+      fallbackLng : 'en',
+      debug : false
+    });
+  });
+  `;
+
+const TRANSLATION_TEMPLATE = `{
+  "title": "Aurelia I18N"
+}`;
+
+module.exports = {
+  IMPORT_SECTION,
+  CONFIGURE_SECTION,
+  TRANSLATION_TEMPLATE
+}
